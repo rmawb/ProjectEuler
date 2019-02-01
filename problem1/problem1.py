3f0d04062570b8ff69dc8b3e1686ff522d1f283c
rmawb <robertmauban@gmail.com>
2019-01-31 19:33:00 -0800

    added solution to problem 1

---
+++ problem1/problem1.py
@@ -0,0 +1,13 @@
+n = 1000
+s = 0
+for x in range(0, n, 3):
+    if x % 5 == 0:
+        s += x/2
+    else:
+        s += x
+for x in range(0, n, 5):
+    if x % 3 == 0:
+        s += x/2
+    else:
+        s += x
+print(int(s))
